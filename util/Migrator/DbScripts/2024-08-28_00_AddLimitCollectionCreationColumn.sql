897d48f1d0ad44a468797c23d3e67a46adf0237f
Addison Beck <github@addisonbeck.com>
2024-08-28 15:04:04 -0400

    Handle new references in `Organization_ReadAbilities`

--- util/Migrator/DbScripts/2024-08-28_00_AddLimitCollectionCreationColumn.sql
+++ util/Migrator/DbScripts/2024-08-28_00_AddLimitCollectionCreationColumn.sql
@@ -304,3 +304,34 @@ BEGIN
     )
 END
 GO
+
+CREATE OR ALTER PROCEDURE [dbo].[Organization_ReadAbilities]
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        [Id],
+        [UseEvents],
+        [Use2fa],
+        CASE
+        WHEN [Use2fa] = 1 AND [TwoFactorProviders] IS NOT NULL AND [TwoFactorProviders] != '{}' THEN
+            1
+        ELSE
+            0
+        END AS [Using2fa],
+        [UsersGetPremium],
+        [UseCustomPermissions],
+        [UseSso],
+        [UseKeyConnector],
+        [UseScim],
+        [UseResetPassword],
+        [UsePolicies],
+        [Enabled],
+        [LimitCollectionCreationDeletion], -- Deprecated https://bitwarden.atlassian.net/browse/PM-10863
+        [LimitCollectionCreation],
+        [AllowAdminAccessToAllCollectionItems]
+    FROM
+        [dbo].[Organization]
+END
+GO
